cf4aadd212411c98662cf49db31f1764df3b573c
aeventre <aeventre@buffalo.edu>
2024-12-01 16:23:10 -0500

    Switched to a python package for simplicity

--- ros2_ws/src/hexapod_com_calculator/setup.py
+++ ros2_ws/src/hexapod_com_calculator/setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import find_packages
 
-ext_modules = [
-    Pybind11Extension(
-        "com_calculator",
-        ["src/com_calculator.cpp", "src/bindings.cpp"],
-    ),
-]
+package_name = 'hexapod_com_calculator'
 
 setup(
-    name="hexapod_com_calculator",
-    version="0.1.0",
-    author="Alec Ventresca",
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
-    packages=["hexapod_com_calculator"],
-    zip_safe=False,
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['setuptools', 'numpy', 'scipy'],  # Add any dependencies here
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='A package for calculating the center of mass of a hexapod robot.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'com_calculator = hexapod_com_calculator.com_node:main',
+        ],
+    },
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
 )
